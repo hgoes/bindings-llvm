7844cf44cec70772753b910b752a4bce0d2d37cb
Henning Günther <guenther@forsyte.at>
2013-04-19 15:20:20 +0200

    Use type families for LoopBase class

--- LLVM/FFI/Loop.hs
+++ LLVM/FFI/Loop.hs
@@ -14,16 +14,24 @@ import LLVM.FFI.CPP
 import Foreign.Ptr
 import Foreign.C
 
-class LoopBaseC t blk loop where
-  loopGetDepth :: Ptr t -> IO CUInt
-  loopGetHeader :: Ptr t -> IO (Ptr blk)
-  loopGetParent :: Ptr t -> IO (Ptr loop)
-  loopContainsLoop :: Ptr t -> Ptr loop -> IO Bool
-  loopContainsBlock :: Ptr t -> Ptr blk -> IO Bool
-  loopGetSubLoops :: Ptr t -> IO (Ptr (Vector (Ptr loop)))
-  loopGetBlocks :: Ptr t -> IO (Ptr (Vector (Ptr blk)))
+#include "Helper.h"
 
-instance LoopBaseC (LoopBase BasicBlock Loop) BasicBlock Loop where
+SPECIALIZE_VECTOR(Loop)
+
+class LoopBaseC t where
+  type LoopBaseBlock t
+  type LoopBaseLoop t
+  loopGetDepth :: Ptr t -> IO CUInt
+  loopGetHeader :: Ptr t -> IO (Ptr (LoopBaseBlock t))
+  loopGetParent :: Ptr t -> IO (Ptr (LoopBaseLoop t))
+  loopContainsLoop :: Ptr t -> Ptr (LoopBaseLoop t) -> IO Bool
+  loopContainsBlock :: Ptr t -> Ptr (LoopBaseBlock t) -> IO Bool
+  loopGetSubLoops :: Ptr t -> IO (Ptr (Vector (Ptr (LoopBaseLoop t))))
+  loopGetBlocks :: Ptr t -> IO (Ptr (Vector (Ptr (LoopBaseBlock t))))
+
+instance LoopBaseC (LoopBase BasicBlock Loop) where
+  type LoopBaseBlock (LoopBase BasicBlock Loop) = BasicBlock
+  type LoopBaseLoop (LoopBase BasicBlock Loop) = Loop
   loopGetDepth = loopGetDepth_
   loopGetHeader = loopGetHeader_
   loopGetParent = loopGetParent_
@@ -32,7 +40,9 @@ instance LoopBaseC (LoopBase BasicBlock Loop) BasicBlock Loop where
   loopGetSubLoops = loopGetSubLoops_
   loopGetBlocks = loopGetBlocks_
   
-instance LoopBaseC Loop BasicBlock Loop where
+instance LoopBaseC Loop where
+  type LoopBaseBlock Loop = BasicBlock
+  type LoopBaseLoop Loop = Loop
   loopGetDepth = loopGetDepth_
   loopGetHeader = loopGetHeader_
   loopGetParent = loopGetParent_
