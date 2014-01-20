c6f6f3882c3e0e83c7da1c2a348ebc3bf4c367fd
Henning Günther <guenther@forsyte.at>
2014-01-20 09:54:03 +0100

    Add a helper module which exports all LLVM modules

---
+++ LLVM/FFI.hs
@@ -0,0 +1,84 @@
+module LLVM.FFI
+       (module LLVM.FFI.APFloat
+       ,module LLVM.FFI.APInt
+       ,module LLVM.FFI.AliasAnalysis
+       ,module LLVM.FFI.ArrayRef
+       ,module LLVM.FFI.BasicBlock
+       ,module LLVM.FFI.CPP
+       ,module LLVM.FFI.Constant
+       ,module LLVM.FFI.Context
+       ,module LLVM.FFI.DebugLoc
+       ,module LLVM.FFI.EngineBuilder
+       ,module LLVM.FFI.ErrorCode
+       ,module LLVM.FFI.ExecutionEngine
+       ,module LLVM.FFI.Function
+       ,module LLVM.FFI.GenericValue
+       ,module LLVM.FFI.IPList
+       ,module LLVM.FFI.Instruction
+       ,module LLVM.FFI.Loop
+       ,module LLVM.FFI.MachineCodeInfo
+       ,module LLVM.FFI.MemoryBuffer
+       ,module LLVM.FFI.Module
+       ,module LLVM.FFI.OOP
+       ,module LLVM.FFI.OStream
+       ,module LLVM.FFI.OwningPtr
+       ,module LLVM.FFI.Pass
+       ,module LLVM.FFI.Pass.Haskell
+       ,module LLVM.FFI.PassInfo
+       ,module LLVM.FFI.PassManager
+       ,module LLVM.FFI.PassManagerBuilder
+       ,module LLVM.FFI.SMDiagnostic
+       ,module LLVM.FFI.SetVector
+       ,module LLVM.FFI.SmallVector
+       ,module LLVM.FFI.StringRef
+       ,module LLVM.FFI.Target
+       ,module LLVM.FFI.TargetMachine
+       ,module LLVM.FFI.Transforms.IPO
+       ,module LLVM.FFI.Transforms.Scalar
+       ,module LLVM.FFI.Twine
+       ,module LLVM.FFI.Type
+       ,module LLVM.FFI.Use
+       ,module LLVM.FFI.User
+       ,module LLVM.FFI.Value) where
+
+import LLVM.FFI.APFloat
+import LLVM.FFI.APInt
+import LLVM.FFI.AliasAnalysis
+import LLVM.FFI.ArrayRef
+import LLVM.FFI.BasicBlock
+import LLVM.FFI.CPP
+import LLVM.FFI.Constant
+import LLVM.FFI.Context
+import LLVM.FFI.DebugLoc
+import LLVM.FFI.EngineBuilder
+import LLVM.FFI.ErrorCode
+import LLVM.FFI.ExecutionEngine
+import LLVM.FFI.Function
+import LLVM.FFI.GenericValue
+import LLVM.FFI.IPList
+import LLVM.FFI.Instruction
+import LLVM.FFI.Loop
+import LLVM.FFI.MachineCodeInfo
+import LLVM.FFI.MemoryBuffer
+import LLVM.FFI.Module
+import LLVM.FFI.OOP
+import LLVM.FFI.OStream
+import LLVM.FFI.OwningPtr
+import LLVM.FFI.Pass
+import LLVM.FFI.Pass.Haskell
+import LLVM.FFI.PassInfo
+import LLVM.FFI.PassManager
+import LLVM.FFI.PassManagerBuilder
+import LLVM.FFI.SMDiagnostic
+import LLVM.FFI.SetVector
+import LLVM.FFI.SmallVector
+import LLVM.FFI.StringRef
+import LLVM.FFI.Target
+import LLVM.FFI.TargetMachine
+import LLVM.FFI.Transforms.IPO
+import LLVM.FFI.Transforms.Scalar
+import LLVM.FFI.Twine
+import LLVM.FFI.Type
+import LLVM.FFI.Use
+import LLVM.FFI.User
+import LLVM.FFI.Value
