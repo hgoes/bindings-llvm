75e257396f3d96a5e3ad69165c67a713ba73c0d6
Henning Günther <guenther@forsyte.at>
2014-09-25 14:32:41 +0200

    Fix number of switch cases in LLVM<3.1

--- LLVM/FFI/Instruction.hs
+++ LLVM/FFI/Instruction.hs
@@ -563,7 +563,7 @@ switchInstGetCases sw = do
            val <- switchInstGetCaseValue sw i
            suc <- terminatorInstGetSuccessor sw (fromIntegral i)
            return (val,suc)
-       ) [0..(no-1)]
+       ) [0..(no-2)]
 #endif
 
 getElementPtrInstGetNumIndices :: Ptr GetElementPtrInst -> IO Integer
